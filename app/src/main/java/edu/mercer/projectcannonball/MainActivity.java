af319bffc531c62a723a08876c72e21c0df9e518
dcmaysse <david.may@live.mercer.edu>
2015-04-24 10:20:01 +0900

    Change true to false for testing

--- app/src/main/java/edu/mercer/projectcannonball/MainActivity.java
+++ app/src/main/java/edu/mercer/projectcannonball/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            return false;
         }
 
         return super.onOptionsItemSelected(item);
