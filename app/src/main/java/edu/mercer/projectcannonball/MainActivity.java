904decf2f210043dc78f0d1cb823826278c1fa89
msddjd <mike.sdj.dumas@gmail.com>
2015-04-23 21:26:49 -0400

    Changed boolean in onOptionsItemSelected back to true.

--- app/src/main/java/edu/mercer/projectcannonball/MainActivity.java
+++ app/src/main/java/edu/mercer/projectcannonball/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return false;
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
